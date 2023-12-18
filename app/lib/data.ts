682484fec9064b561dd41eb677c978e50d5cb338
mshakhriyorov <mshakhriyorov8@gmail.com>
2023-12-18 16:55:58 +0500

    removed unnecessary comments

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,20 +13,10 @@ import {
 import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
 
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
